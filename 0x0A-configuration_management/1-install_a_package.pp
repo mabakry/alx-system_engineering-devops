368eda07edbf07b708c88723487c8594694b0996
mabakry <mabakry@gmail.com>
2024-02-23 16:48:01 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install flask from pip3.
 
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip',
+exec { 'install_flask':
+command => 'pip3 install flask==2.1.0',
+path    => ['/usr/bin'],
+require => Package['python3'],
 }
