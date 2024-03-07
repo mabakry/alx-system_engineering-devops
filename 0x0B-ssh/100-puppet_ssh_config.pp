4583955843cf7e0c9144dc830c24cc367b7e009d
mabakry <mabakry@gmail.com>
2024-03-07 23:08:20 +0000

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#  changes the ssh configuration file using Puppet.
+
+file_line { 'Turn off password auth':
+  ensure => 'present',
+    path => '/etc/ssh/ssh_config',
+    line => '    PasswordAuthentication no',
+  match  => '#     PasswordAuthentication yes',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+    path => '/etc/ssh/ssh_config',
+    line => '    IdentityFile ~/.ssh/holberton',
+}
